8cc1994a6ea627adcf31cfa69fc1d817a62768cc
Jui-Yen Huang <45046364+EdnaHuang@users.noreply.github.com>
2024-01-15 17:23:43 -0500

    Add files via upload

--- Statistical_Script_Data/Analysis_FGFRcKO_Ephys_dendrite/src/Develop_FGFRcKO_Ephys_Function_posthoc_20230731.R
+++ Statistical_Script_Data/Analysis_FGFRcKO_Ephys_dendrite/src/Develop_FGFRcKO_Ephys_Function_posthoc_20230731.R
@@ -66,40 +66,24 @@ boot_model_extract_raw = function(x) {
   return(boot_rep)
 }
 
-##plot raw data and bootstrapping result -Facet by layer ------------------
 
-plot_raw_boot_facet_sex = function(x, var_name = x, title = x,
-                                     y_breaks = c(1:6 * 0.2), 
-                                     y_limits = c(0, 1), 
-                                     legend_position = "none") {
-  # define label
+
+##merge sex then plot raw data color by sex-----------
+merge_sex_plot_raw_color_sex = function(var, data = df_lite, title = var,
+                                        y_breaks = c(1:6 * 0.2), 
+                                        y_limits = c(0, 1), 
+                                        legend_position = "none") {
   
-  sex.label = c("Female", "Male")
-  names(sex.label) = c("F", "M")
-  
-  
-  
-   # extract replicates
-  mod_boot = boot_model_extract_raw(x)
-  
-  # use original data set instead
-  # https://tibble.tidyverse.org/reference/subsetting.html
-  # the subsetting behavior is somewhat different in tibbles lol
-  df_lite$yvar = df_lite[, x, drop = TRUE]
- 
-  # marginal effect point estimates and CI 
-  emms = emmip(mod_boot, group ~ f.age | sex, CIs = TRUE, plotit = FALSE)
-  emm_plot = ggplot(emms, aes(x = f.age, y = yvar, color = group)) +
-    geom_pointrange(aes(ymin = LCL, ymax = UCL), size = 1,
-                    position = position_dodge (width =0.5),
-                    shape = 16, fatten = 1) +
-    geom_beeswarm(data = df_lite, dodge.width = 0.5, alpha = 0.7,
-                  shape = 1, size = 1) +
+  df_lite %>%
+    select(all_of(var), subject_id, sex, f.age, group) %>%
+    ggplot(aes(x = f.age, y = get(var), group = group, color = sex)) +
+    ggbeeswarm::geom_beeswarm(shape = 19, size = 1,
+                              dodge.width = 0.5, alpha = 0.3) +
+    stat_summary(fun.data = "mean_cl_boot", size = 0.5,linewidth = 1,
+                 position = position_dodge(width = 0.5),fatten = 1.5) +
     scale_y_continuous(breaks = y_breaks, limits = y_limits) +
-    facet_wrap(~ sex,labeller = labeller(sex =sex.label),
-               strip.position = "bottom") +
-    labs(x = "", y= "", color = "group") +
-    ggtitle(title)+
+    #facet_wrap(~ layer, labeller = labeller(layer = layer.label)) +
+    labs(x = "", y = "", title = title) + 
     theme(strip.text = element_text(size = 10), 
           strip.background = element_blank(),
           strip.placement = ("outside"),
@@ -112,17 +96,15 @@ plot_raw_boot_facet_sex = function(x, var_name = x, title = x,
           axis.ticks.x = element_blank(),
           plot.background = element_blank(),
           plot.margin = unit(c(0,0,0,0), 'cm'))+
-    scale_color_d3() ->p
+    scale_color_nejm() ->p
   
   return(p)
   
 }
 
 
-## report main of group, sex, age to compare ANOVA table before boots -------------
-x = "s_epsc_amp"
-var_name = x
 
+## report main of group, sex, age to compare ANOVA table before boots -------------
 report_main_effect = function(x, var_name = x) {
   
   # extract bootstrapped coef by model class
@@ -159,36 +141,16 @@ report_main_effect = function(x, var_name = x) {
   table1 = kable(anova,caption = "ANOVA table before bootstrapping", digits = 3)
   print(table1)
   
-  table2 = kable(list(emm_param_group, emm_param_age, emm_param_sex), digits = 3)
+  table2 = kable(emm_param_group, caption ="Main effect after bootstrapping", digits = 3)
   print(table2)
-}
-
-
-
-
-## report group inter layer ---------
-report_group_int_sex = function(x, var_name = x) {
   
-  # extract bootstrapped coef by model class
-  if (x %in% names(case_boot_list)) {
-    mod_boot = lmer_boot_extract(x)
-  } else {
-    mod_boot = lm_boot_extract(x)
-  }
+  table3 = kable(emm_param_age ,digits = 3)
+  print(table3)
   
-  # simple comparisons
-  emm = emmeans(mod_boot, ~ group* sex)
+  table4 = kable(emm_param_sex, digits = 3)
+  print(table4)
   
-  #compare
-  emm_contr_group = contrast(emm, method = "pairwise")
-  emm_param_group = as.data.frame(model_parameters(
-    emm_contr_group, centrality = c("median", "mean"), test = "pd"))
-  emm_param_group$pval = pd_to_p(emm_param_group$pd)
-  emm_param_group$Median.1 = NULL
   
-  # print formatted tables in sequence
-  table = kable (emm_param_group, digits = 3)
-  print(table)
 }
 
 
@@ -207,7 +169,7 @@ report_inter_age_group_sex = function(x, var_name = x) {
   # age
   emm_contr_age = contrast(emm, method = "pairwise", simple = "f.age")
   emm_param_age = as.data.frame(model_parameters(
-          emm_contr_age, centrality = c("median", "mean"), test = "pd"))
+    emm_contr_age, centrality = c("median", "mean"), test = "pd"))
   emm_param_age$pval = pd_to_p(emm_param_age$pd)
   emm_param_age$Median.1 = NULL
   
@@ -226,52 +188,15 @@ report_inter_age_group_sex = function(x, var_name = x) {
   emm_param_sex$Median.1 = NULL
   
   # print formatted tables in sequence
-  table = kable (list(emm_param_group,emm_param_age,emm_param_sex), digits = 3)
-  print(table)
-}
-
-
-##merge layer then plot raw data and bootstrapping result------------------
-
-merge_sex_plot_raw_boot = function(x, var_name = x,title = x,
-                                     y_breaks = c(1:6 * 0.2), 
-                                     y_limits = c(0, 1), 
-                                     legend_position = "none") {
+  table1 = kable (emm_param_group,
+                  caption = "Post-hoc comparison with bootstrapping output", digits = 3)
+  print(table1)
   
-  # extract replicates
-  mod_boot = boot_model_extract_raw(x)
+  table2 = kable (emm_param_age,digits = 3)
+  print(table2)
   
-  # use original data set instead
-  # https://tibble.tidyverse.org/reference/subsetting.html
-  # the subsetting behavior is somewhat different in tibbles lol
-  df_lite$yvar = df_lite[, x, drop = TRUE]
-  
-  # marginal effect point estimates and CI 
-  emms = emmip(mod_boot, group ~ f.age, CIs = TRUE, plotit = FALSE)
-  emm_plot = ggplot(emms, aes(x = f.age, y = yvar, color = group, shape = 1, size = 1)) +
-    geom_pointrange(aes(ymin = LCL, ymax = UCL), size = 1,
-                    position = position_dodge (width =0.5),
-                    shape = 16, fatten = 1) +
-    geom_beeswarm(data = df_lite, dodge.width = 0.5, alpha = 0.7,
-                  shape = 1, size = 1) +
-    scale_y_continuous(breaks = y_breaks, limits = y_limits) +
-    labs(x = "", y= "", color = "group") +
-    ggtitle(title)+
-    theme(strip.text = element_text(size = 10), 
-          strip.background = element_blank(),
-          strip.placement = ("outside"),
-          legend.text = element_text(color = "black", size  = "10"),
-          legend.position = legend_position, legend.title = element_blank(),
-          axis.text.x = element_text(color = "black", size = "10"),
-          axis.line.x.bottom = element_line (linewidth = 1/2),
-          axis.text.y = element_text(color = "black", size = "10"),
-          axis.line.y = element_line(linewidth = 1/2), 
-          axis.ticks.x = element_blank(),
-          plot.background = element_blank(),
-          plot.margin = unit(c(0,0,0,0), 'cm'))+
-    scale_color_d3() ->p
-  
-  return(p)
+  table3 = kable (emm_param_sex,digits = 3)
+  print(table3)
   
 }
 
@@ -307,9 +232,43 @@ merge_sex_report_age_int_group = function(x, var_name = x) {
   emm_param_group$Median.1 = NULL
   
   # print formatted tables in sequence
-  table = kable (list(emm_param_group, emm_param_age), digits = 3)
+  table1 = kable (emm_param_group,
+                  caption = "Merge-sex-Post-hoc comparison with bootstrapping output", digits = 3)
+  print(table1)
+  
+  table2 = kable (emm_param_age,digits = 3)
+  print(table2)
+}
+
+
+
+
+
+## report group inter layer ---------
+report_group_int_sex = function(x, var_name = x) {
+  
+  # extract bootstrapped coef by model class
+  if (x %in% names(case_boot_list)) {
+    mod_boot = lmer_boot_extract(x)
+  } else {
+    mod_boot = lm_boot_extract(x)
+  }
+  
+  # simple comparisons
+  emm = emmeans(mod_boot, ~ group* sex)
+  
+  #compare
+  emm_contr_group = contrast(emm, method = "pairwise")
+  emm_param_group = as.data.frame(model_parameters(
+    emm_contr_group, centrality = c("median", "mean"), test = "pd"))
+  emm_param_group$pval = pd_to_p(emm_param_group$pd)
+  emm_param_group$Median.1 = NULL
+  
+  # print formatted tables in sequence
+  table = kable (emm_param_group, digits = 3)
   print(table)
 }
+
 
 
 
